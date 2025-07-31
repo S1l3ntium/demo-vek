336bbcce37a1848dc310ce4cf76a4efd3b1421b6
Denis Chichmarev <denis.chichmarev@gmail.com>
2025-07-31 13:40:25 +0300

    develop - fix/fix-on-mobile

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,16 @@ function setupScroller() {
 	const row1 = document.querySelector('.row1')
 	const row2 = document.querySelector('.row2')
 
-	// Находим ширину прокрутки строк
 	const row1Width = row1.scrollWidth
 	const row2Width = row2.scrollWidth
 	const viewportWidth = window.innerWidth
 
-	// Расчёт смещения в px
-	const move1 = row1Width - viewportWidth
-	const move2 = row2Width - viewportWidth
+	const move1 = row1Width > viewportWidth ? row1Width - viewportWidth : 0
+	const move2 = row2Width > viewportWidth ? row2Width - viewportWidth : 0
 
-	// Самая длинная прокрутка
 	const maxScroll = Math.max(move1, move2)
-
-	// Общая длина скролла (можно добавить множитель для замедления)
 	const scrollLength = maxScroll * 1.5
 
-	// ScrollTrigger с pin на всей секции
 	ScrollTrigger.create({
 		trigger: scroller,
 		start: 'top top',
@@ -28,10 +22,9 @@ function setupScroller() {
 		pin: true,
 		scrub: true,
 		anticipatePin: 1,
+		invalidateOnRefresh: true,
 		onUpdate: self => {
 			const progress = self.progress
-
-			// Двигаем обе строки пропорционально их смещению
 			gsap.to(row1, { x: -move1 * progress, duration: 0, overwrite: true })
 			gsap.to(row2, { x: -move2 * progress, duration: 0, overwrite: true })
 		},
